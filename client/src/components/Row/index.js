45596d8b769ad2732e2a89c439317e57f60df35e
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-09-26 19:53:00 -0700

    finished template routing for pages

--- client/src/components/Row/index.js
+++ client/src/components/Row/index.js
@@ -1,7 +1,8 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 
 function Row(props) {
-  return <div className={`row${props.fluid ? "-fluid" : ""}`} {...props} />;
+	return <div className={`row${props.fluid ? "-fluid" : ""}`} {...props} />;
 }
 
 export default Row;
--- client/src/pages/CreateUser.js
+++ client/src/pages/CreateUser.js
@@ -1,32 +1,32 @@
 import React from "react";
-//import Header from "../components/Header";
+import Header from "../components/Header";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
 
 function CreateUser() {
-    return (
-        <div>
-            <Header>
+	return (
+		<div>
+			<Header>
 
-            </Header>
-            <Container>
-                <Row>
-                    <Col size="md-12">
-                    <h1> This will be the create a profile page</h1>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                    <p>
+			</Header>
+			<Container>
+				<Row>
+					<Col size="md-12">
+						<h1> This will be the create a profile page</h1>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<p>
                         We can enter forms here to begin tests
-                    </p>
-                    </Col>
-                </Row>
-            </Container>
+						</p>
+					</Col>
+				</Row>
+			</Container>
 
-        </div>
-    );
+		</div>
+	);
 }
 
 export default CreateUser;
