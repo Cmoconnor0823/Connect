9abad5945624be6c48d65b5beb50400b386ad3fd
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-09-26 17:05:20 -0700

    added placeholder components and beginning to make needed pages

---
+++ client/src/pages/CreateUser.js
@@ -0,0 +1,32 @@
+import React from "react";
+//import Header from "../components/Header";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Col from "../components/Col";
+
+function CreateUser() {
+    return (
+        <div>
+            <Header>
+
+            </Header>
+            <Container>
+                <Row>
+                    <Col size="md-12">
+                    <h1> This will be the create a profile page</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    <p>
+                        We can enter forms here to begin tests
+                    </p>
+                    </Col>
+                </Row>
+            </Container>
+
+        </div>
+    );
+}
+
+export default CreateUser;
