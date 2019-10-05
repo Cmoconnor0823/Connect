58d68a99b9d761a7623ffffd9f39ab9fd495b661
Jonbonbroski <jonbonbroski@gmail.com>
2019-10-05 11:03:44 -0700

    routes basic

---
+++ Server/routes/messageRoute.ts
@@ -0,0 +1,21 @@
+const express = require("express")
+const router = express.Router();
+const Messages = require("../models/Messages");
+const db = require('../config/index')
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+router.get('/',(req,res)=>{
+
+    Messages.findAll()
+    .then(messages =>{
+
+        console.log(messages);
+        res.sendStatus(200);
+    })
+    .catch(err => console.log(err));
+}) 
+
+};
