621f642d4d93398f4db94d9ae3a3f5c131d90475
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-10-06 16:57:46 -0700

    changed card shadow color and footer colors

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,33 +4,33 @@ import "./style.css";
 
 function Footer() {
 	return (
-				<footer className="footer navbar-expand-lg navbar-dark">
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul className="navbar-nav mr-auto pages float-left">
-						<li className="nav-item active">
-							<a className="navbar-brand" href="/home">
-								Home
+		<footer className="footer navbar-expand-lg">
+			<div className="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul className="navbar-nav mr-auto pages float-left">
+					<li className="nav-item active">
+						<a className="navbar-brand" href="/home">
+							Home
 			</a>
-						</li>
-						<li className="nav-item">
-							<a className="navbar-brand" href="/login">
-								Log In
+					</li>
+					<li className="nav-item">
+						<a className="navbar-brand" href="/login">
+							Log In
 			</a>
-						</li>
-						<li className="nav-item">
-							<a className="navbar-brand" href="/createuser">
-								Create a Profile
+					</li>
+					<li className="nav-item">
+						<a className="navbar-brand" href="/createuser">
+							Create a Profile
 			</a>
-						</li>
-						{/* <li>
+					</li>
+					{/* <li>
 				<a className="navbar-brand" href="/">
 				Home
 				</a> 
 			</li>*/}
-				<div className="navbar-brand copyright col-lg-3" id="copyright"><h4>© KonnectD 2019</h4></div>
-					</ul>
-					</div>
-				</footer>
+					<div className="navbar-brand copyright col-lg-3" id="copyright"><h4>© KonnectD 2019</h4></div>
+				</ul>
+			</div>
+		</footer>
 	);
 }
 
