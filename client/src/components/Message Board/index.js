e01a4703ed26ea39c917c2e6ade0da93670518d8
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-10-14 16:40:38 -0700

    added horizontal scroll for mobile view points for message table

--- client/src/components/Message Board/index.js
+++ client/src/components/Message Board/index.js
@@ -4,58 +4,58 @@ import { FormBtn, Input, TextArea } from "../Form";
 import "./style.css";
 import axios from 'axios';
 
-class MessageCards  extends Component {
+class MessageCards extends Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-            author:"",
-            subject:"",
-            newMessage:"",
-            messages:[],
-            
-			
-			};
-		}
-	
-		componentDidMount() {
-			axios.get("/api/messages")
+			author: "",
+			subject: "",
+			newMessage: "",
+			messages: [],
+
+
+		};
+	}
+
+	componentDidMount() {
+		axios.get("/api/messages")
 			.then((res) => {
 				// console.log(users)
 				console.log("request sent")
 				console.log(res.data);
 				this.setState({
-					messages:res.data
+					messages: res.data
 				})
 			});
-			}
-		handleNewMessage = event => {
-			// event.preventDefault();
-			console.log("clicked")
-			axios.post("/api/createMessage",
-            {
-                message: this.state.newMessage,
-                author: this.state.author,
-                subject: this.state.subject
+	}
+	handleNewMessage = event => {
+		// event.preventDefault();
+		console.log("clicked")
+		axios.post("/api/createMessage",
+			{
+				message: this.state.newMessage,
+				author: this.state.author,
+				subject: this.state.subject
 			})
-			.then((res)=> {
+			.then((res) => {
 				console.log("new Message created")
 				this.setState({
-                    newMessage:"",
-                    author:"",
-                    subject:"",
+					newMessage: "",
+					author: "",
+					subject: "",
 				})
 			})
-		}
-		  //https://medium.com/@aghh1504/1-simple-react-todo-list-52186b62976b
-		  //Check the above link to continue?
+	}
+	//https://medium.com/@aghh1504/1-simple-react-todo-list-52186b62976b
+	//Check the above link to continue?
 
-		  handleInputChange = event => {
-			const { name, value } = event.target;
-			this.setState({
-				[name]: value
-			});
-		};
+	handleInputChange = event => {
+		const { name, value } = event.target;
+		this.setState({
+			[name]: value
+		});
+	};
 
 	render() {
 		return (
@@ -64,60 +64,70 @@ class MessageCards  extends Component {
 					<h4> Message Board </h4>
 				</CardTitle>
 				<CardBody>
-					<Table hover responsive="sm">
+				<div class="panel-body">
+					<Table hover bordered responsive="sm" className="table-responsive">
 						<thead>
-                            <tr>
-                                <th>Created on</th>
-                                <th>Author</th>
-                                <th>subject</th>
-                                <th>Message</th>
-                            </tr>
-                        </thead>
+							<tr>
+								<th>Created on</th>
+								<th>Author</th>
+								<th>Subject</th>
+								<th>Message</th>
+							</tr>
+						</thead>
 
-						
-						
+
+
 						<tbody>
-                        
-								{/* <tr>{this.state.messages.id} */}
-								{/* <th>Date Due</th> */}
-								{this.state.messages.map(messages =>
-								<tr key ={messages.id}>
-								<td>{messages.createdAt}</td>
-                                <td>{messages.author}</td>
-                                <td>{messages.subject}</td>
-								{/* <td>{messages.deadline}</td> */}
-								<td>{messages.message}</td>
-								</tr>
-							
-						)}	
 
-							
-					
+							{/* <tr>{this.state.messages.id} */}
+							{/* <th>Date Due</th> */}
+							{this.state.messages.map(messages =>
+								<tr key={messages.id}>
+									<td>{messages.createdAt}</td>
+									<td>{messages.author}</td>
+									<td>{messages.subject}</td>
+									{/* <td>{messages.deadline}</td> */}
+									<td>{messages.message}</td>
+								</tr>
+
+							)}
+
+
+
 						</tbody>
 					</Table>
-                    <Input
-                        value={this.state.author}
-                        onChange={this.handleInputChange}
-                        name="author"
-                        placeholder="author(optional)"
-						/>
-                    <Input
-                        value={this.state.subject}
-                        onChange={this.handleInputChange}
-                        name="subject"
-                        placeholder="subject (optional)"
-                    />
-					 <Input
+					</div>
+				
+					<Input
+						value={this.state.author}
+						onChange={this.handleInputChange}
+						name="author"
+						placeholder="Author (Optional)"
+					/>
+					<Input
+						value={this.state.subject}
+						onChange={this.handleInputChange}
+						name="subject"
+						placeholder="Subject (Optional)"
+					/>
+					<TextArea
+						type="text"
+						placeholder="Write a message to your team (Optional)"
+						name="newMessage"
+						onChange={this.handleInputChange}
+						value={this.state.newMessage}
+					/>
+					{/* <Input
 						value={this.state.newMessage}
 						onChange={this.handleInputChange}
 						name="newMessage"
 						placeholder="Write a message to your team(Optional)"
-					/>
-					
-					<FormBtn 
+					/> */}
+
+					<FormBtn
 						enabled={!(this.state.message)}
 						onClick={this.handleNewMessage}
-						> Add a new Message</FormBtn>
+					> Add a new Message</FormBtn>
 				</CardBody>
 			</Card>
 		);
