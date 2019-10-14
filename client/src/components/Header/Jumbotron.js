29ec0ddecc419f8878da25dde26375b3aa3c3123
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-10-14 16:20:58 -0700

    fixed image issue in jumbotron

--- client/src/components/Header/Jumbotron.js
+++ client/src/components/Header/Jumbotron.js
@@ -6,12 +6,11 @@ import './style.css';
 
 const Jumbo = (props) => {
 	return (
-		<Jumbotron className="jumbotron-billboard">
-			<Container>
+		<Jumbotron className="jumbotron jumbotron-billboard">
 			<Row>
 			{/*  Try applying this to the image
 			"backgroundSize:'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' */}
-				<Col>
+				<Col className="ml-3 mr-3">
 			<div className="imgb"></div>
 				<h3 className="text-center p-5">
 					<img src={image} className="img rounded pull-left" alt="Logo" ></img>
@@ -20,7 +19,6 @@ const Jumbo = (props) => {
 					
 				</Col>
 			</Row>
-			</Container>
 		</Jumbotron>
 	);
 };
