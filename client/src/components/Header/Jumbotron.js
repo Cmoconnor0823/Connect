c86ea5666f873387d1911258db1bf2090eb95cc0
skish1987 <seankish1@gmail.com>
2019-10-14 18:12:14 -0700

    changes made

--- client/src/components/Header/Jumbotron.js
+++ client/src/components/Header/Jumbotron.js
@@ -12,12 +12,12 @@ const Jumbo = (props) => {
 			{/*  Try applying this to the image
 			"backgroundSize:'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' */}
 				<Col>
-			<div className="imgb"></div>
-				<h3 className="text-center p-5">
-					<img src={image} className="img rounded pull-left" alt="Logo" ></img>
-				
-						Welcome To KonnectD, a Business Management Tool for Project Management and Records.</h3>
+					<div className="imgb"></div>
+					<h3 className="text-center p-5">
+						<img src={image} className="img rounded pull-left" alt="Logo" ></img>
 					
+							Welcome to KonnectD, a Business Management Tool for Project Management and Records
+					</h3>
 				</Col>
 			</Row>
 			</Container>
